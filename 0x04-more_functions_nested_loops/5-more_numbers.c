894ee85d7339332d4fb0fa5710de37c928afa61a
b-lemy <lemabrian1234@gmail.com>
2022-09-15 07:36:21 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,27 @@
-#include "main.h"
+#include"main.h"
 
 /**
-* print_most_numbers -  prints the numbers except 2 and 4.
+ * more_numbers - print 0 - 14 ten times and you
+ *		can only use _putchar three times
+ *
+ * Return: Always 0 (Success)
 */
+
 void more_numbers(void)
 {
-	int i;
-	char c;
+	int num, row, count;
 
-	for (i = 0; i < 10; i++)
+	for (row = 1; row <= 10; ++row)
 	{
-		for (c = 0; c <= 14; c++)
+		for (count = 0; count <= 14; ++count)
 		{
-			if (c > 9)
-			_putchar((c / 10) + '0');
-			_putchar((c % 10) + '0');
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
 		}
 		_putchar('\n');
 	}
