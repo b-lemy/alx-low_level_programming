397ce907407ed6a5c572ce66cfb1b483f351874c
b-lemy <lemabrian1234@gmail.com>
2022-09-23 01:39:07 -0700

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,16 @@
+/**
+*print_number - prints a number
+*@n: the number
+**/
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print(n / 10);
+	putchar(n % 10 + '0');
+}
